89397bade07543e53d60e8433ddc222764aa2855
mohandalhlwany-eng <mohandalhlwany@gmail.com>
2026-09-16 04:28:54 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -254,15 +254,3 @@ async function saveBookingToTosupabase(customerData, fromVal, toVal) {
     }
 }
 });
-window.goBack = function(sectionId) {
-    // إخفاء كل الأقسام اللي فيها كلاس section أو تبدأ بـ section-
-    document.querySelectorAll('.section, [id^="section-"]').forEach(sec => {
-        sec.classList.add('section-hidden');
-    });
-    // إظهار القسم المحدد بشكل مؤكد وسلس
-    const target = document.getElementById(sectionId);
-    if (target) {
-        target.classList.remove('section-hidden');
-        target.style.display = 'block';
-    }
-};
